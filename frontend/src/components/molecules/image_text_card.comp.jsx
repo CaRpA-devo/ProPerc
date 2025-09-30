822e0d9cd88219606f0d37d6f1ec97a930bce66f
CaRpA-devo <carpa-devo@web.de>
2025-09-30 15:07:02 +0200

    update

--- frontend/src/components/molecules/image_text_card.comp.jsx
+++ frontend/src/components/molecules/image_text_card.comp.jsx
@@ -1,11 +1,9 @@
 import { ArrowButton } from "../atoms/arrowbutton.comp";
-import { CardWrapper } from "../atoms/cardwrapper.org";
 
 export function ImageTextCard({
   imageSrc,
   category,
   description,
-
   containerClass = "",
   imageClass = "",
   categoryClass = " ",
@@ -13,54 +11,45 @@ export function ImageTextCard({
   reverse = false,
   listItems = [],
   buttonText = "",
-  styleContainer = "",
 }) {
   return (
-    <CardWrapper
-      className={` md:max-w-220 w-full 
-     ${containerClass}`}
+    <div
+      className={`flex flex-col gap-36  justify-between md:flex-row ${containerClass}  ${
+        reverse ? "md:flex-row-reverse" : ""
+      }`}
     >
+      {/* Bild oben auf Mobile, links auf Desktop */}
       <div
-        className={`flex flex-col gap-8   justify-center items-center md:flex-row   ${
-          reverse ? "md:flex-row-reverse" : ""
-        }`}
-      >
-        {/* Bild oben auf Mobile, links auf Desktop */}
+        className={`h-48 w-full rounded-xl md:h-100 md:w-100 bg-cover bg-center ${imageClass}`}
+        style={{ backgroundImage: `url(${imageSrc})` }}
+      ></div>
+
+      {/* Textbereich */}
+      <div className="gap-4 flex text-center p-0 max-w-120 justify-center flex-col items-center sm:p-8 sm:flex-1 sm:text-start sm:items-start">
         <div
-          className={`h-48 w-full rounded-xl md:h-100 md:w-100 bg-cover bg-center ${imageClass}`}
-          style={{ backgroundImage: `url(${imageSrc})` }}
-        ></div>
-
-        {/* Textbereich */}
-        <div className="gap-4 flex text-center p-0 justify-center flex-col items-center sm:p-8 sm:flex-1 sm:text-start sm:items-start">
-          <div
-            className={`text-3xl font-bold text-primary tracking-wide uppercase ${categoryClass}`}
-          >
-            {category}
-          </div>
-
-          <p
-            className={`text-lg text-base-content/80  leading-relaxed mb-8 ${descriptionClass}`}
-          >
-            {description}
-          </p>
-
-          {/* Dynamische Liste */}
-          {listItems.length > 0 && (
-            <ul className="flex flex-col items-center  list-disc list-inside marker:text-secondary  marker:text-xl">
-              {listItems.map((item, index) => (
-                <li
-                  key={index}
-                  className="text-base-content/70 leading-relaxed"
-                >
-                  {item}
-                </li>
-              ))}
-            </ul>
-          )}
-          <ArrowButton text={buttonText} />
+          className={`text-3xl font-bold text-primary tracking-wide uppercase ${categoryClass}`}
+        >
+          {category}
         </div>
+
+        <p
+          className={`text-lg text-base-content/80  leading-relaxed mb-8 ${descriptionClass}`}
+        >
+          {description}
+        </p>
+
+        {/* Dynamische Liste */}
+        {listItems.length > 0 && (
+          <ul className="flex flex-col items-center  list-disc list-inside marker:text-secondary  marker:text-xl">
+            {listItems.map((item, index) => (
+              <li key={index} className="text-base-content/70 leading-relaxed">
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
+        <ArrowButton text={buttonText} />
       </div>
-    </CardWrapper>
+    </div>
   );
 }
