4103ac61962678df192bca334f33aa3040d84331
CaRpA-devo <carpa-devo@web.de>
2025-09-30 12:39:01 +0200

    Bug fixing

--- frontend/src/components/pages/index.page.jsx
+++ frontend/src/components/pages/index.page.jsx
@@ -1,6 +1,7 @@
 import { DefaultLayout } from "../layouts/default.layout";
 import { LandingCard } from "../organisms/landingcard.org.jsx";
 import { SectionWrapper } from "../atoms/sectionwrapper.comp";
+import { Features } from "../organisms/features.org.jsx";
 
 export function IndexPage() {
   return (
@@ -8,24 +9,7 @@ export function IndexPage() {
       {/* TODO Metatags einfügen */}
 
       <DefaultLayout withHero>
-        <SectionWrapper
-          className="bg-first-bg p-8 text-center "
-          position="center"
-        >
-          <div className="gap-8 flex flex-col  max-w-240 ">
-            <h2 className=" text-xl sm:text-4xl">
-              Deine Ernährung. Dein Fortschritt. Deine App.
-            </h2>
-            <p className="text-sm  sm:text-xl">
-              Starte mit ProPerc in eine gesunde und motivierende Zukunft. Ob
-              Rezepte, persönliche Ernährungspläne oder smartes Tracking hier
-              bekommst du alles, was du für einen nachhaltigen Lifestyle
-              brauchst. Klar, einfach und individuell auf dich zugeschnitten.
-            </p>
-          </div>
-        </SectionWrapper>
-
-        <LandingCard />
+        <Features />
       </DefaultLayout>
     </>
   );
